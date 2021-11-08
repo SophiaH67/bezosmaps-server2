126ac08abdb307864e0f8df447dfe0c9ddf6bdba
marnixah <marnix@hagehoeve.com>
2021-11-08 21:00:39 +0100

    fix: redis?

--- lib/block_walkable.py
+++ lib/block_walkable.py
@@ -9,6 +9,7 @@ def set_block_walkable(x, y, z, walkable=True):
     """
     Set a block to be walkable or not.
     """
+    print(f"Setting {x}:{z}:{y} to {1 if walkable else 0}")
     return r.setbit(f"{x}:{z}", y, 1 if walkable else 0)
 
 def get_block_walkable(x, y, z):
@@ -17,10 +18,8 @@ def get_block_walkable(x, y, z):
     """
     return r.getbit(f"{x}:{z}", y)
 
-loaded = r.get('loaded')
-if loaded is None:
-    print("Setting up redis...")
-    r.set('loaded', 'true')
-    blocks = db.session.query(Block).all()
-    for block in blocks:
-        set_block_walkable(block.x, block.y, block.z, block.name in walkable_blocks)
+print("Setting up redis...")
+r.set('loaded', 'true')
+blocks = db.session.query(Block).all()
+for block in blocks:
+    set_block_walkable(block.x, block.y, block.z, block.name in walkable_blocks)
